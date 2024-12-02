1ba9e11f5a3cc25c3a88bf8cc823bd1fce5089cf
Lina <linabattikha@gmail.com>
2024-12-01 23:47:58 -0800

    run updated with some waterbirds code

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 import sys
 import json
+import os
+
+# Add the 'src' directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.join(current_dir, "src")
+sys.path.append(src_path)
 
 from etl import (
     retrieve_adult_data,
@@ -7,30 +13,54 @@ from etl import (
     retrieve_covid_data,
     gmm_covid,
     plot_pca_gmm_covid,
-    kmeans_adults
+    kmeans_adults,
+    retrieve_features,
+    run_waterbirds, 
+    run_census, 
+    
 )
+    
+    
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if 'census_model' in args: 
+        print("census")
 
-def main(targets): 
-    # Load data parameters from JSON configuration file
-    with open('etl/data-params.json') as fh:
-        data_params = json.load(fh)
-    
-    if 'gmm_adults' in targets: 
+    if 'waterbirds_features' in args:
+        with open("config/waterbirds_extract_features.json", "r") as file:
+            config = json.load(file)
+        retrieve_features(**config)
+
+    if 'waterbirds_model' in args:
+        print("wb")
+
+
+    # COME BACK AND UPDATE
+    if 'gmm_adults' in args: 
+        with open('config/data-params.json') as fh:
+            data_params = json.load(fh)
+        
         gmm_adults(data_params["gmm_adult_ts"])
-    
-    if 'gmm_covid' in targets: 
+
+    # COME BACK AND UPDATE
+    if 'gmm_covid' in args:
+        with open('config/data-params.json') as fh:
+            data_params = json.load(fh)
+        
         gmm_covid(
         data_params["covid_fp"],
         data_params["replace_num"],
         data_params["gmm_covid_ts"]
     )
-    
-    if 'plot_gmm_covid' in targets: 
-        plot_pca_gmm_covid()
         
-    if 'kmeans_adults' in targets:
+    # COME BACK AND UPDATE
+    if 'plot_gmm_covid' in args: 
+        with open('config/data-params.json') as fh:
+            data_params = json.load(fh)
+        plot_pca_gmm_covid()
+
+    # COME BACK AND UPDATE
+    if 'kmeans_adults' in args:
+        with open('config/data-params.json') as fh:
+            data_params = json.load(fh)
         kmeans_adults()
-    
-    
-if __name__ == '__main__':
-    main()
